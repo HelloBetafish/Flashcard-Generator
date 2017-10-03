7aea3486836fcb9e313f80c59985b236fa5955be
HelloBetafish <bethany.pfeister@gmail.com>
2017-10-03 12:27:15 -0700

    tried to fix spacing from Sublime to Git

--- index.js
+++ index.js
@@ -12,9 +12,9 @@ inquirer
       name: "type"
     }
   ]).then(function(response) {
-  	if (response.type === "Basic"){
-  	  inquirer.prompt([
-  	    {
+    if (response.type === "Basic"){
+      inquirer.prompt([
+        {
   	      type: "input",
   	      message: "What is the question?",
   	      name: "question"
@@ -25,18 +25,18 @@ inquirer
   	      name: "answer"
   	    }
   	  ]).then(function(response){
-  	  	var Basic = BasicCard(response.question, response.answer);
-  	  	console.log(Basic);
-  	  	fs.appendFile("log.txt", "---------------------------\r\nBasic Flaschcard:\r\nFront: " +
+        var Basic = BasicCard(response.question, response.answer);
+        console.log(Basic);
+        fs.appendFile("log.txt", "---------------------------\r\nBasic Flaschcard:\r\nFront: " +
   	  	  Basic.front + "\r\nBack: " + Basic.back + "\r\n", function(err) {
   	  	  if(err){
-  	  	  	return console.log(err);
+            return console.log(err);
   	  	  }
-  	  	});
-  	  });
+        });
+      });
   	}
   	else if (response.type === "Cloze"){
-  	  	inquirer.prompt([
+      inquirer.prompt(
   	    {
   	      type: "input",
   	      message: "What is the question?",
@@ -51,10 +51,10 @@ inquirer
   	  	var Cloze = ClozeCard(response.question, response.cloze);
   	  	console.log(Cloze);
   	  	fs.appendFile("log.txt", "---------------------------\r\nCloze Flashcard:\r\nFull text: " +
-  	  	 Cloze.fullText + "\r\nCloze: " + Cloze.cloze + "\r\nPartial Text: " + Cloze.partial +
-  	  	 "\r\n", function(err) {
+  	  	  Cloze.fullText + "\r\nCloze: " + Cloze.cloze + "\r\nPartial Text: " + Cloze.partial +
+  	  	  "\r\n", function(err) {
   	  	  if(err){
-  	  	  	return console.log(err);
+            return console.log(err);
   	  	  }
   	  	});
   	  });
